9aedc81c67f5cee2bc9f70fd262af4c7bbff3c14
TusharJain-cloud <tj471258@gmail.com>
2025-01-30 10:31:29 -0500

    Kadane's Algorithm

---
+++ 4_maximum_subarray.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        currSum = 0
+        maxSum = nums[0]
+
+        for val in nums:
+            if currSum < 0:
+                currSum = 0
+            
+            currSum += val
+            maxSum = max(maxSum, currSum)
+
+        return maxSum
+        
