0d68432ed46761f9d3b5bff660df0b377919807e
TusharJain-cloud <tj471258@gmail.com>
2025-05-17 10:55:32 -0400

    Create 98_CountAllSubseq.py

---
+++ 98_CountAllSubseq.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countSubsequenceWithTargetSum(self, nums, k):
+        #your code goes here
+        res = 0
+
+        def backtrack(i, cur_sum):
+            nonlocal res
+            if cur_sum == k:
+                res += 1
+                return 
+
+            if i == len(nums):
+                return
+            
+            backtrack(i + 1, cur_sum + nums[i])
+            backtrack(i + 1, cur_sum)
+        
+        backtrack(0,0)
+        return res
+                
